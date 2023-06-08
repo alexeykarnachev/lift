6bfdc87f861d3d23c1f57cb0a9aca4e3426516b2
Alexey Karnachev <alekseykarnachev@gmail.com>
2023-06-09 01:35:24 +0400

    update kinematics (gravity + collisions)

--- src/game.rs
+++ src/game.rs
@@ -22,6 +22,11 @@ enum State {
     JumpLanding,
 }
 
+#[derive(Default)]
+struct Debug {
+    show_rigid_colliders: bool,
+}
+
 const MAX_N_ENTITIES: usize = 1024;
 
 pub struct Game {
@@ -44,11 +49,14 @@ pub struct Game {
     behaviours: [Behaviour; MAX_N_ENTITIES],
     look_dirs: [f32; MAX_N_ENTITIES],
     are_grounded: [bool; MAX_N_ENTITIES],
+    are_kinematic: [bool; MAX_N_ENTITIES],
 
     frame_animators: [Option<FrameAnimator>; MAX_N_ENTITIES],
     rigid_colliders: [Option<Rect>; MAX_N_ENTITIES],
     attack_colliders: [Option<Rect>; MAX_N_ENTITIES],
     sprites: [Option<XYWH>; MAX_N_ENTITIES],
+
+    debug: Debug,
 }
 
 impl Game {
@@ -70,6 +78,10 @@ impl Game {
             frame_atlas_image_fp,
         ));
 
+        let debug = Debug {
+            show_rigid_colliders: true,
+        };
+
         Self {
             dt: 0.0,
             prev_upd_time: Instant::now(),
@@ -88,17 +100,24 @@ impl Game {
             next_states: [State::Idle; MAX_N_ENTITIES],
             look_dirs: [1.0; MAX_N_ENTITIES],
             are_grounded: [false; MAX_N_ENTITIES],
+            are_kinematic: [false; MAX_N_ENTITIES],
 
             frame_animators: [None; MAX_N_ENTITIES],
             rigid_colliders: [None; MAX_N_ENTITIES],
             attack_colliders: [None; MAX_N_ENTITIES],
             sprites: [None; MAX_N_ENTITIES],
+
+            debug,
         }
     }
 
     pub fn start(&mut self) {
         self.new_knight_player(Vec2::new(0.0, 0.0));
         self.new_wolf_ai(Vec2::new(40.0, 0.0));
+        self.new_rigid_collider(
+            Pivot::TopCenter(Vec2::new(0.0, 0.0)),
+            Vec2::new(1000.0, 50.0),
+        );
 
         while !self.input.should_quit {
             self.update_input();
@@ -107,21 +126,38 @@ impl Game {
         }
     }
 
-    pub fn update_input(&mut self) {
+    pub fn get_rigid_collider(&mut self, idx: usize) -> Option<Rect> {
+        if let Some(rect) = self.rigid_colliders[idx] {
+            return Some(rect.translate(self.positions[idx]));
+        }
+
+        None
+    }
+
+    pub fn get_attack_collider(&mut self, idx: usize) -> Option<Rect> {
+        if let Some(rect) = self.attack_colliders[idx] {
+            return Some(rect.translate(self.positions[idx]));
+        }
+
+        None
+    }
+
+    fn update_input(&mut self) {
         for event in self.event_pump.poll_iter() {
             self.input.handle_event(&event);
         }
         self.input.update();
     }
 
-    pub fn update_world(&mut self) {
+    fn update_world(&mut self) {
         self.dt = self.prev_upd_time.elapsed().as_nanos() as f32 / 1.0e9;
         self.update_behaviours();
         self.update_frame_animators();
+        self.update_kinematics();
         self.prev_upd_time = Instant::now();
     }
 
-    pub fn update_renderer(&mut self) {
+    fn update_renderer(&mut self) {
         self.renderer.clear_queue();
         self.renderer
             .set_camera(self.camera.position, self.camera.get_view_size());
@@ -139,6 +175,15 @@ impl Game {
                     flip,
                 );
 
+                self.renderer.push_primitive(primitive);
+            }
+
+            if let (Some(rect), true) = (
+                self.get_rigid_collider(idx),
+                self.debug.show_rigid_colliders,
+            ) {
+                let primitive =
+                    DrawPrimitive::world_rect(rect, Color::red(0.2));
                 self.renderer.push_primitive(primitive);
             }
         }
@@ -162,9 +207,52 @@ impl Game {
         }
     }
 
+    fn update_kinematics(&mut self) {
+        for idx in 0..self.n_entities {
+            if !self.are_kinematic[idx] {
+                continue;
+            }
+
+            self.velocities[idx].y -= self.gravity * self.dt;
+            self.positions[idx] += self.velocities[idx].scale(self.dt);
+
+            if let Some(collider) = self.get_rigid_collider(idx) {
+                let mut is_grounded = false;
+
+                for other_idx in 0..self.n_entities {
+                    if idx == other_idx {
+                        continue;
+                    }
+
+                    if let (Some(other_collider), false) = (
+                        self.get_rigid_collider(other_idx),
+                        self.are_kinematic[other_idx],
+                    ) {
+                        let mtv = collider.collide_aabb(other_collider);
+                        self.positions[idx] += mtv;
+
+                        if mtv.y.abs() > 0.0 {
+                            self.velocities[idx].y = 0.0;
+                        }
+
+                        if mtv.x.abs() > 0.0 {
+                            self.velocities[idx].x = 0.0;
+                        }
+
+                        is_grounded |= mtv.y > 0.0;
+                    }
+                }
+
+                self.are_grounded[idx] = is_grounded;
+            }
+        }
+    }
+
     fn update_knight_player_behaviour(&mut self, idx: usize) {
         use sdl2::keyboard::Keycode::*;
         use State::*;
+
+        let jump_speed = 150.0;
 
         let is_jump_action = self.input.key_is_pressed(W);
         let is_roll_action = self.input.key_is_pressed(LCtrl);
@@ -180,7 +268,7 @@ impl Game {
             Idle => {
                 if is_jump_action {
                     self.set_curr_state(idx, JumpUp);
-                    self.velocities[idx].y += 100.0;
+                    self.velocities[idx].y += jump_speed;
                 } else if is_step_action {
                     self.set_curr_state(idx, Run);
                 }
@@ -190,7 +278,7 @@ impl Game {
                     self.set_curr_state(idx, Roll);
                 } else if is_jump_action {
                     self.set_curr_state(idx, JumpUp);
-                    self.velocities[idx].y += 100.0;
+                    self.velocities[idx].y += jump_speed;
                 } else if is_step_action {
                     self.do_immediate_step(idx, 100.0, dir);
                 } else {
@@ -258,12 +346,12 @@ impl Game {
                 self.sprites[idx] = Some(frame.sprite);
                 self.rigid_colliders[idx] = frame.get_mask(
                     "rigid",
-                    Pivot::BotCenter(position),
+                    Pivot::BotCenter(Vec2::zeros()),
                     false,
                 );
                 self.attack_colliders[idx] = frame.get_mask(
                     "attack",
-                    Pivot::BotCenter(position),
+                    Pivot::BotCenter(Vec2::zeros()),
                     false,
                 );
             }
@@ -299,10 +387,7 @@ impl Game {
         }
     }
 
-    pub fn new_knight_player(
-        &mut self,
-        position: Vec2<f32>,
-    ) -> Option<usize> {
+    fn new_entity(&mut self) -> Option<usize> {
         if self.n_entities == MAX_N_ENTITIES {
             return None;
         }
@@ -310,28 +395,36 @@ impl Game {
         let idx = self.n_entities;
         self.n_entities += 1;
 
-        self.positions[idx] = position;
-        self.behaviours[idx] = Behaviour::KnightPlayer;
-        self.curr_states[idx] = State::Idle;
-        self.frame_animators[idx] = Some(self.frame_atlas.new_animator());
-
-        Some(idx)
+        return Some(idx);
     }
 
-    pub fn new_wolf_ai(&mut self, position: Vec2<f32>) -> Option<usize> {
-        if self.n_entities == MAX_N_ENTITIES {
-            return None;
+    fn new_knight_player(&mut self, position: Vec2<f32>) {
+        if let Some(idx) = self.new_entity() {
+            self.positions[idx] = position;
+            self.behaviours[idx] = Behaviour::KnightPlayer;
+            self.curr_states[idx] = State::Idle;
+            self.frame_animators[idx] =
+                Some(self.frame_atlas.new_animator());
+            self.are_kinematic[idx] = true;
         }
+    }
 
-        let idx = self.n_entities;
-        self.n_entities += 1;
+    pub fn new_wolf_ai(&mut self, position: Vec2<f32>) {
+        if let Some(idx) = self.new_entity() {
+            self.positions[idx] = position;
+            self.behaviours[idx] = Behaviour::WolfAI;
+            self.curr_states[idx] = State::Idle;
+            self.frame_animators[idx] =
+                Some(self.frame_atlas.new_animator());
+            self.are_kinematic[idx] = true;
+        }
+    }
 
-        self.positions[idx] = position;
-        self.behaviours[idx] = Behaviour::WolfAI;
-        self.curr_states[idx] = State::Idle;
-        self.frame_animators[idx] = Some(self.frame_atlas.new_animator());
-
-        Some(idx)
+    pub fn new_rigid_collider(&mut self, pivot: Pivot, size: Vec2<f32>) {
+        if let Some(idx) = self.new_entity() {
+            let rect = Rect::from_pivot(pivot, size);
+            self.rigid_colliders[idx] = Some(rect);
+        }
     }
 
     fn do_immediate_step(&mut self, idx: usize, speed: f32, dir: f32) {
